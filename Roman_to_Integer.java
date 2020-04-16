47a249c490c2f9a5116fa76e9891d84be9dee1d8
suraj vishwakarma <34504675+vsuraj3@users.noreply.github.com>
2020-04-15 21:08:02 -0400

    update leetcode problem #13

--- Roman_to_Integer.java
+++ Roman_to_Integer.java
@@ -1,5 +1,41 @@
 package leetode;
+import java.util.HashMap;
+import java.util.Scanner;
 
+class romanInt{
+public int romanToInt(String s) 
+{
+	int res=0;
+	HashMap<Character, Integer> map=new HashMap<Character, Integer>();
+	map.put('I', 1);
+	map.put('V', 5);
+	map.put('X', 10);
+	map.put('L', 50);
+	map.put('C', 100);
+	map.put('D', 500);
+	map.put('M', 1000);
+	for(int i=0;i<s.length();i++)
+	{
+		if(map.get(s.charAt(i))>map.get(s.charAt(i-1)))
+		{
+			res+=map.get(s.charAt(i))-2*map.get(s.charAt(i-1));
+		}
+		else 
+		{
+			res+=map.get(s.charAt(i));
+		}
+	}
+	return res;
+}
+}
 public class Roman_to_Integer {
+	
+	public static void main(String args[]) 
+	{
+		Scanner sc=new Scanner(System.in);
+		String s=sc.next();
+		romanInt obj=new romanInt();
+		obj.romanToInt(s);
+	}
 
 }
