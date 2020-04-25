3dced9b5a727132281edac91c3e9164efcecc15b
suraj vishwakarma <34504675+vsuraj3@users.noreply.github.com>
2020-04-25 18:49:56 -0400

    Leetcode Solution #771

---
+++ Jewels_Stone.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int numJewelsInStones(String J, String S) {
+         int sum=0;
+        HashSet<Character> h = new HashSet<Character>();
+        
+        for(int i=0; i<J.length(); i++)
+            h.add(J.charAt(i));
+        
+        for(int i=0; i<S.length(); i++)
+            if(h.contains(S.charAt(i)))
+                sum++;
+        return sum;
+        
+    }
+}
