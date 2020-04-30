12e396cde9b7af588198e8e548433cab2f1ee7c7
suraj vishwakarma <34504675+vsuraj3@users.noreply.github.com>
2020-04-30 00:55:16 -0400

    Leetcode Problem #22

---
+++ Generate_Parentheses.java
@@ -0,0 +1,25 @@
+class Solution {
+    public List<String> generateParenthesis(int n) 
+    {
+     List<String> res=new ArrayList<String>();
+     backtrack(res,"",0,0,n);
+     return res;
+    }
+    
+    public void backtrack(List<String> res,String s, int open,int close, int max) 
+    {
+    	if(s.length()==max*2)
+    	{
+    		res.add(s);
+    		return;
+    	}
+    	
+    	if(open<max) 
+    	{
+    		backtrack(res, s+"(", open+1, close, max);
+    	}
+    	if(close<open) {
+    		backtrack(res, s+")", open, close+1, max);
+    	}
+    }
+}
