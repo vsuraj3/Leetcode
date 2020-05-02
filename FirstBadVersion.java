024251b0970b92c85ece9c05c48a7a3c9a2746b4
suraj vishwakarma <34504675+vsuraj3@users.noreply.github.com>
2020-05-02 00:42:53 -0400

    Leetcode Problem #278

--- FirstBadVersion.java
+++ FirstBadVersion.java
@@ -4,12 +4,12 @@ public class Solution extends VersionControl {
         int right=n;
         while(left<right){
             int mid=left+(right-left)/2;
-            if(!isBadVersion(mid)){
-                left=mid+1;
-            }else{
+            if(isBadVersion(mid)){
                 right=mid;
+            }else{
+                left=mid+1;
             }
         }
-        return left;
+        return right;
     }
 }
