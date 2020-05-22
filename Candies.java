c329d283bf25cc7fbf1959221d8215e156f24523
suraj vishwakarma <34504675+vsuraj3@users.noreply.github.com>
2020-05-22 17:27:36 -0400

    Leetcode Problem #1413

---
+++ Candies.java
@@ -0,0 +1,15 @@
+class Solution {
+   public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) 
+{
+	java.util.List<Boolean> al=new ArrayList<Boolean>();
+	int max = 0;
+    for (int i : candies) 
+    if (i > max) max = i;
+	for(int i:candies) 
+	{
+		al.add(i+extraCandies>=max);
+	}
+	return al;
+    
+}
+}
