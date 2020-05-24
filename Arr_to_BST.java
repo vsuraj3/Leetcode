182ece42538e5390b740ad102d14532250012fb9
suraj vishwakarma <34504675+vsuraj3@users.noreply.github.com>
2020-05-24 19:22:17 -0400

    Leetcode Problem #108

---
+++ Arr_to_BST.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        if(nums==null || nums.length==0){
+            return null;
+        }
+        
+        return bst(nums,0,nums.length-1);
+        
+    }
+    private TreeNode bst(int nums[], int start, int end){
+
+        if(start>end){
+            return null;
+        }
+        int mid=(start+end)/2;
+        TreeNode curr = new TreeNode(nums[mid]);
+    curr.left=bst(nums,start,mid-1);
+    curr.right=bst(nums,mid+1,end);
+    return curr;
+    
+    }
+
+}
