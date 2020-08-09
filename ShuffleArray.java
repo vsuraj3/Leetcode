0409a51a3fe3d5472adad144e54dc77d66b4c4ce
suraj vishwakarma <34504675+vsuraj3@users.noreply.github.com>
2020-08-08 23:22:25 -0400

    Leetcode Problem #1470

---
+++ ShuffleArray.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] shuffle(int[] nums, int n) 
+    {
+     int i=0;
+     int j=n;
+       int res[]=new int[2*n];
+        int curr=0;
+        while(i<n)
+        {
+         res[curr]=nums[i];
+            curr++;
+            i++;
+            res[curr]=nums[j];
+            curr++;
+            j++;
+            
+        }
+        return res;
+    }
+}
